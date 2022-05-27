f4cc4a839b65506822c7443885b80d39ac7925a4
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-05-27 18:54:49 -0300

    commit

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,17 @@
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const { user } = await signInWithGooglePopup();
+        createUserDocumentFromAuth(user);
+    }
+
     return (
         <div className="sign-in">
             <h2>I already have an account</h2>
+            <button onClick={logGoogleUser}>
+                Sign in with Google
+            </button>
         </div>
     );
 }
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,44 @@
+import {
+    initializeApp
+} from 'firebase/app';
+
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
+} from 'firebase/auth';
+import {
+getFirestore,
+doc,
+getDoc,
+setDoc
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCFJQh32mBIYt5-BYvYZdeioOZdByEJ41g",
+    authDomain: "crwn-clothing-db-75622.firebaseapp.com",
+    projectId: "crwn-clothing-db-75622",
+    storageBucket: "crwn-clothing-db-75622.appspot.com",
+    messagingSenderId: "914977159390",
+    appId: "1:914977159390:web:9c64d9d061f86036c1ea4f"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  
+}
