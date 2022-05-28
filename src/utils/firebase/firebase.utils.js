546fa6ec425a3e7dc91f9f5f739fd4ee18a0abb7
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-05-28 14:25:11 -0300

    create auth component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,10 +9,10 @@ import {
     GoogleAuthProvider
 } from 'firebase/auth';
 import {
-getFirestore,
-doc,
-getDoc,
-setDoc
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -38,7 +38,28 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
-  const userDocRef = doc(db, 'users', userAuth.uid);
+    const userDocRef = doc(db, 'users', userAuth.uid);
 
-  
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const {
+            displayName,
+            email
+        } = userAuth;
+
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userDocRef;
 }
