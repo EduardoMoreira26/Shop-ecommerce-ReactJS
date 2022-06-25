6258435397a8b6d220b1495a57ac40aa0f531976
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-06-25 18:23:55 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component"
+import Shop from "./routes/shop/shop.component"
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication";
-
-const OutLet = () => {
-	return (
-		<h1>
-			I am outlet page
-		</h1>
-	);
-}
 
 const App = () => {
 	return ( 
 		<Routes>
 			<Route path="/" element={<Navigation/>}>
 				<Route index element={<Home />}/>
-				<Route path="shop" element={<OutLet />} />
+				<Route path="shop" element={<Shop />} />
 				<Route path="auth" element={<Authentication />} />
 			</Route>
 		</Routes>
