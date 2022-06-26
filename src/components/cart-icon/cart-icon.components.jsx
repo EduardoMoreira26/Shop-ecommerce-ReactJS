ecac279111375843b94950f1ab93d3a9fe943ff7
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-06-26 12:59:21 -0300

    commit

--- src/components/cart-icon/cart-icon.components.jsx
+++ src/components/cart-icon/cart-icon.components.jsx
@@ -4,14 +4,14 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss'
 
 const CartIcon = () => {
-	const { isCartOpen ,setIsCartOpen } = useContext(CartContext);
+	const { isCartOpen, setIsCartOpen, cartItems } = useContext(CartContext);
 
 	const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
 	return (
 		<div className="cart-icon-container">
 			<ShoppingIcon className='shopping-icon' onClick={toggleIsCartOpen} />
-			<span className='item-count'>0</span>
+			<span className='item-count'>{cartItems.length}</span>
 		</div>
 	)
 }
